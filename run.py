4523d81f13d06479993b147411dc080653e9d345
DaniCarmo <>
2023-10-11 10:51:10 +0000

    add welcome message

--- run.py
+++ run.py
@@ -19,3 +19,22 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('book_repository')
 
+
+def welcome_message():
+    """
+    Welcome message providing the main menu of the library.
+    """
+    clear_tmnl()
+    print("Welcome to Your Leaving Cert Library!\n")
+    print("To find and check out a book, please select an option below.\n")
+    print(colored(("(1) Search by Subject"), "green"))
+    print(colored(("(2) Search by Publisher"), "green"))
+    print(colored(("(3) Search by Keyword"), "green"))
+
+    while True:
+        welcome_answer = input("\n")
+        if welcome_answer not in ("1", "2", "3"):
+            print(colored(("Oops! Please choose option 1, 2 or 3"), "red"))
+        else:
+            break
+    return welcome_answer
