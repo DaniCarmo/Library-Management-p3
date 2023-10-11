66b5596ec71db18d43902591d3c34195e65cbd30
DaniCarmo <>
2023-10-11 11:27:20 +0000

    add functions to search by options provided

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ def welcome_message():
     print("To find and check out a book, please select an option below.\n")
     print(colored(("(1) Search by Subject"), "green"))
     print(colored(("(2) Search by Publisher"), "green"))
-    print(colored(("(3) Search by Keyword"), "green"))
+    print(colored(("(3) Search by Book Title"), "green"))
 
     while True:
         welcome_answer = input("\n")
@@ -38,3 +38,47 @@ def welcome_message():
         else:
             break
     return welcome_answer
+
+
+def search_subject(sheet_name, subject):
+    """
+    when user searches for book by subject
+    """
+    return search_google_sheet(sheet_name, 'Subject', subject)
+
+
+def search_publisher(sheet_name, publisher):
+    """
+    when user searches for book by publisher
+    """
+    return search_google_sheet(sheet_name, 'Publisher', publisher)
+
+
+def search_title(sheet_name, title):
+    """
+    when user searches for book by keyword
+    """
+    return search_google_sheet(sheet_name, 'Title', book_name)
+
+
+def search_google_sheet(sheet_name, column_heading, search_value):
+    # Open the Google Sheet by name
+    try:
+        sheet = client.open(sheet_name).sheet1
+    except gspread.exceptions.SpreadsheetNotFound:
+        return "Spreadsheet not found"
+    
+    # Get all values from the specified column
+    column_values = sheet.col_values(sheet.find(column_heading).col)
+    
+    # Search for the value in the column and return matching rows
+    matching_rows = [row for row in sheet.get_all_records() if row[column_heading] == search_value]
+    
+    return matching_rows
+
+
+def clear_tmnl():
+    """
+    clears terminal
+    """
+    os.system("clear")
